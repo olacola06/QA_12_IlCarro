3c0a5649e3cc89673cb90c217e803313095e79ec
olacola06 <olamarchen@gmail.com>
2022-05-03 11:51:33 +0300

    03 May

--- QA_12_IlCarro/src/test/java/tests/Registration.java
+++ QA_12_IlCarro/src/test/java/tests/Registration.java
@@ -1,7 +1,8 @@
 package tests;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class Registration extends TestBase {
 
