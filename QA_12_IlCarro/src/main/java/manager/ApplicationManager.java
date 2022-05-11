250a9ea95830a408d446c085572021ecfd24825e
olacola06 <olamarchen@gmail.com>
2022-05-11 09:10:47 +0300

    Add

--- QA_12_IlCarro/src/main/java/manager/ApplicationManager.java
+++ QA_12_IlCarro/src/main/java/manager/ApplicationManager.java
@@ -2,6 +2,7 @@ package manager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,7 +14,11 @@ public class ApplicationManager {
     HelperCar helperCar;
 
     public void setUp(){
-        wd = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("load-extension=C://Tools/4.46.1_0");
+        //wd = new ChromeDriver();
+        wd = new ChromeDriver(options);
         wd.manage().window().maximize();
         wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         wd.navigate().to("https://ilcarro.xyz/search");
